e5a809588d903e5acdc419e10fc5e441baea1e67
InnaMazur <ineska.lat@gmail.com>
2025-11-25 21:07:35 +0300

    js1

---
+++ 01/main1.js
@@ -0,0 +1,9 @@
+let userAge = 40;
+let userNickname = "Inna";
+console.log(userAge);
+console.log(userNickname);
+alert(userNickname);
+userAge = 41;
+userNickname = "Inna2025";
+console.log(userAge);
+console.log(userNickname);
